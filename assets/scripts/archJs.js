fcf3a415fb2c59e86806deef0fd81211ff48bfbf
Gutsnt <charlie.mendoza1337@gmail.com>
2022-06-01 19:47:25 -0400

    -fixed

--- assets/scripts/archJs.js
+++ assets/scripts/archJs.js
@@ -1,6 +1,8 @@
 console.log("hola mundo en js") 
 
 let links = document.querySelectorAll("a");
+let cadena = "hola mundo";
+let mayu= cadena.toUpperCase;
 
 links.forEach(function(links){
 	console.log(links);
