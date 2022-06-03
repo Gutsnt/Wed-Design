91305e32407c384bc301d7df04d3b9c8eb7b1647
Gutsnt <charlie.mendoza1337@gmail.com>
2022-06-03 07:43:59 -0400

    -Praticing Js

--- assets/scripts/archJs.js
+++ assets/scripts/archJs.js
@@ -1,9 +1,37 @@
-console.log("hola mundo en js") 
+class Book{
 
-let links = document.querySelectorAll("a");
-let cadena = "hola mundo";
-let mayu= cadena.toUpperCase;
+	constructor(title,author,year,gender){
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.gender = gender;
+	}
 
-links.forEach(function(links){
-	console.log(links);
-})
+	bookInfo(){
+		return `${title} escrito por ${author} 
+		del genero ${gender} en el año ${year}`
+	}
+
+}
+
+	let  books = []
+
+	while (books.length < 3){
+		let title = prompt(`Introduce el titulo del libro`);
+		let author = prompt(`Introduce el author del libro`);
+		let year = prompt(`Introduce el year del libro`);
+		let gender = prompt(`Introduce el gender del libro`).toLowerCase();
+
+		if( title != `` && author != `` && year.length == 4 &&
+			(gender == `accion` || gender == `terror`)){
+			
+			books.push(new Book(title,author,year,gender))
+		}
+
+	}
+
+	const showAllBooks = () =>{
+		console.log(books);
+	}
+
+	showAllBooks();
