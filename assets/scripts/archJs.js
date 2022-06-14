54610abb83f63a1ca46f502184cc87b2638f8888
Gutsnt <charlie.mendoza1337@gmail.com>
2022-06-13 20:04:22 -0400

    prac

--- assets/scripts/archJs.js
+++ assets/scripts/archJs.js
@@ -77,10 +77,11 @@ const parent = document.getElementById('parent')
 console.log(parent.firstElementChild);
 console.log(parent.hasChildNodes());
 */
-
+/*
 const list = document.getElementById('list')
 const newElement = document.createElement('li')
 newElement.textContent = "i'm a new element"
+*/
 //list.children[0].insertAdjacentElement('beforebegin',newElement)
 //list.children[0].insertAdjacentElement('afterend',newElement)
 //list.insertBefore(newElement, list.children[0])
@@ -144,7 +145,7 @@ button.addEventListener('click',() =>{
 	xhr.send()
 })
 */
-
+/*
 JAVAX III
 const form = document.getElementById('form')
 form.addEventListener('submit',(e) =>{
@@ -163,3 +164,52 @@ const sendData = (data) => {
 
 
 }
+*/
+
+//CallBacks
+/*
+Funcion que se ejecuta a traves de otra funcion
+No son asincronos
+*/
+
+//Promesas
+
+//Objeto con dos ComeBacks internos
+/*
+const users = [{id: 1, name: 'Dorian' },{id: 2, name: 'Laura' },
+				{id: 3, name: 'Carlos' }]
+const emails = [{id: 1, email: 'dorian@gmail.com' },
+				{id: 2, email: 'dorian@gmail.com' }]
+
+const getUser= (id) => {
+	const user = users.find(user => user.id == id )
+	return promise = new Promise((resolve,reject)=>{
+		if (!user) reject(`Doesn't exist an user with id ${id}`)
+		else resolve(user)
+	})
+	
+}
+
+const getEmail = (user) => {
+	const email = emails.find(email => email.id == user.id)
+
+	return promise = new Promise((resolve,reject)=>{
+		if (!email) reject(`${user.name} hasn't email `)
+		else resolve({
+			id: user.id,
+			name: user.name,
+			email: email.email 
+		})
+	})
+}
+
+getUser(3)
+	.then(user => getEmail(user))
+	.then(res=>console.log(res))
+	.catch(err =>console.log(err))
+
+getUser(2)
+	.then(getEmail)
+	.then(console.log)
+	.catch(console.log)
+*/
