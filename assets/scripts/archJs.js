b01967dc7679d8972b63e7b392a2e35ebe175183
Charlie Mendoza <charlie.mendoza1337@gmail.com>
2022-06-21 22:04:48 -0400

    API Online/Offline

--- assets/scripts/archJs.js
+++ assets/scripts/archJs.js
@@ -707,3 +707,29 @@ fileInput.addEventListener('change', (e) => {
 	addEventListener('visibilitychange', () 
 	=> document.visibilityState === 'visible' ? video.play() : video.pause())
 */
+/*
+	const alert = document.getElementById('alert')
+
+	addEventListener('online', (e) => {
+	    setAlert(1)
+	})
+
+	addEventListener('offline', (e) => {
+	    setAlert(0)
+	})
+
+	const setAlert = (status) => {
+	    alert.classList.remove('alert--online')
+	    alert.classList.remove('alert--offline')
+
+	    status === 0 ?
+	        setTimeout(() => {
+	            alert.textContent = "Ups, it seems you're offline"
+	            alert.classList.add('alert--offline')
+	        }, 100) :
+	        setTimeout(() => {
+	            alert.textContent = "Great! You're online again!"
+	            alert.classList.add('alert--online')
+	        }, 100)
+	}
+*/
